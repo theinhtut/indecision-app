9ff0075f8e043a1ef8e3b13d37405c556634ee68
Thein Htut <theinhtut.dec@gmail.com>
2019-03-17 23:32:44 +0800

    Installed webpack and fixed @babel/

---
+++ src/app3.js
@@ -0,0 +1,10 @@
+// import './utils';
+// import { square, add } from './utils';
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(8,2));
+
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(25));
+console.log(canDrink(25));
