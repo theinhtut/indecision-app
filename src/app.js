fd0aed236f4377a86e69cfcd82f6d9bb5c1e76ce
Thein Htut <theinhtut.dec@gmail.com>
2019-03-19 16:21:13 +0800

    Added SCSS and setup styling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TheindecisionApp from './components/TheindecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+//import csSs from 'file.css';
 
 ReactDOM.render(<TheindecisionApp />, document.getElementById('app'));
--- src/components/TheindecisionApp.js
+++ src/components/TheindecisionApp.js
@@ -26,7 +26,7 @@ export default class TheindecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    
+
     this.setState(() => ({
       selectedOption: option
     }));
@@ -76,19 +76,24 @@ export default class TheindecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption handleAddOption={this.handleAddOption} />
-        <OptionModal 
-        selectedOption={this.state.selectedOption}
-        handleClearSelectedOption={this.handleClearSelectedOption}
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption handleAddOption={this.handleAddOption} />
+          </div>
+        </div>
+
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
         />
       </div>
     );
